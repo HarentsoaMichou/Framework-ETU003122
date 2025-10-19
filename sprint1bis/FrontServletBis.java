9db20035bb692bef60779a6a203a8bcc25f18fe0
HarentsoaMichou <harentsoamichou@gmail.com>
2025-10-19 17:39:57 +0300

    Create FrontServletBis.java

---
+++ sprint1bis/FrontServletBis.java
@@ -0,0 +1,32 @@
+package sprint1bis;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.RequestDispatcher;
+
+
+public class FrontServletBis {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response) 
+            throws IOException, ServletException {
+                String path = request.getRequestURI().substring(request.getContextPath().length());
+                boolean resourceExists = getServletContext().getResource(path) != null;
+
+            if (resourceExists) {
+                RequestDispatcher dispatcher = getServletContext().getNamedDispatcher("default");
+                dispatcher.forward(request, response);
+            } else {
+        response.setContentType("text/html;charset=UTF-8");
+        response.getWriter().println("<h1>URL demandee : " + path + "</h1>");
+            }
+    }
+    
+}
