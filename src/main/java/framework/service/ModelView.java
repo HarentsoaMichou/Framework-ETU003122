ca2277dc058a9e1e0e69fa0114a212be6073ecf9
HarentsoaMichou <harentsoamichou@gmail.com>
2026-07-29 20:30:08 +0300

    Sprint 11
    Gestion de sessions (@Session)

--- src/main/java/framework/service/ModelView.java
+++ src/main/java/framework/service/ModelView.java
@@ -7,6 +7,7 @@ public class ModelView {
     
     private String view;
     private Map<String, Object> data;
+    private Map<String, Object> session = new HashMap<>();
     
     public ModelView() {
         this.data = new HashMap<>();
@@ -16,6 +17,11 @@ public class ModelView {
         this.view = view;
         this.data = new HashMap<>();
     }
+
+    public Map<String, Object> getSession() {
+        return session;
+    }
+
 
     public String getView() {
         return view;
@@ -35,5 +41,21 @@ public class ModelView {
     
     public void addObject(String key, Object value) {
         this.data.put(key, value);
+    }
+
+    public void setSession(Map<String, Object> session) {
+    this.session = session;
+    }
+    public void addSession(String key, Object value) {
+        this.session.put(key, value);
+    }
+    public Object getSessionObject(String key) {
+        return this.session.get(key);
+    }
+    public void removeSession(String key) {
+        this.session.remove(key);
+    }
+    public boolean hasSessionKey(String key) {
+        return this.session.containsKey(key);
     }
 }
